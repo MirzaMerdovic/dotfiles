61d767e94f15c10570516ecae0f2ecf4bd573c93
Mirza Merdovic <mirzamerdovic@gmail.com>
2026-09-05 00:36:18 +0200

    Add caffeinate aliases

--- dot_bashrc.d/30-aliases.sh
+++ dot_bashrc.d/30-aliases.sh
@@ -2,6 +2,10 @@
 alias brcrld='source ~/.bashrc'
 alias brcedit='nano ~/.bashrc'
 
+alias awake-on='systemctl --user start caffeinate.service'
+alias awake-off='systemctl --user stop caffeinate.service'
+alias awake-status='systemctl --user status caffeinate.service --no-pager'
+
 # Navigation
 alias c='clear'
 alias ..='cd ..'
